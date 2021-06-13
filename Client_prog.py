16a7e12a33742835d35bc33e42264ecbc8311105
AishwaryaBirla <53036180+AishwaryaBirla@users.noreply.github.com>
2021-06-13 13:16:15 +0530

    Update Client_prog.py

--- Client_prog.py
+++ Client_prog.py
@@ -1,9 +1,6 @@
 import cv2
-import numpy as np
 import socket
-import sys
 import pickle
-import struct
 
 # cap=cv2.VideoCapture(0)
 clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -34,16 +31,3 @@ while True:
 
 cv2.destroyAllWindows()
 clientsocket.close()
-
-
-'''while True:
-    cap=cv2.VideoCapture(0)
-    ret,frame=cap.read()
-    data = pickle.dumps(frame)
-    message_size = struct.pack("I", len(data))
-    clientsocket.sendall(message_size + data)
-    cv2.imshow('frame', frame)
-    cv2.waitKey(1)
-
-cv2.destroyAllWindows()
-'''
